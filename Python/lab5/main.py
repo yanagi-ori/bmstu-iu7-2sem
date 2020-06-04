10a0488124b96aab8672fce794d7055fa087b108
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-04 13:17:56 +0300

    refactoring

--- Python/lab5/main.py
+++ Python/lab5/main.py
@@ -6,29 +6,30 @@ import pygame
 pygame.init()
 pygame.time.set_timer(pygame.USEREVENT, 3000)
 
-FPS = 20
+FPS = 60
 W = 600
 H = 600
 BLACK = (0, 0, 0)
 OBJECTS = ('./astr1.png', './astr2.png', './nyan.png', './123.png')
-OBJ_SURF = []  # для хранения готовых машин-поверхностей
+OBJ_SURF = []  # для хранения готовых поверхностей
 motion = 'STOP'
 font = pygame.font.Font(pygame.font.match_font('dejavusans'), 36)
 
 # надо установить видео режим до вызова image.load()
 sc = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
 
 for i in range(len(OBJECTS)):
     OBJ_SURF.append(pygame.image.load(OBJECTS[i]).convert_alpha())
 
 
-class Car(pygame.sprite.Sprite):
+class SpaceObjectX(pygame.sprite.Sprite):
     def __init__(self, x, surf, group):
         pygame.sprite.Sprite.__init__(self)
         self.image = surf
         self.rect = self.image.get_rect(center=(x, 0))
         self.add(group)  # добавляем в группу
-        self.speed = randint(1, 2)  # у машин будет разная скорость
+        self.speed = randint(1, 2)  # у объектов будет разная скорость
 
     def update(self):
         if self.rect.y < H:
@@ -37,13 +38,13 @@ class Car(pygame.sprite.Sprite):
             self.kill()
 
 
-class Car1(pygame.sprite.Sprite):
+class SpaceObjectY(pygame.sprite.Sprite):
     def __init__(self, y, surf, group):
         pygame.sprite.Sprite.__init__(self)
         self.image = surf
         self.rect = self.image.get_rect(center=(0, y))
         self.add(group)  # добавляем в группу
-        self.speed = randint(1, 2)  # у машин будет разная скорость
+        self.speed = randint(1, 2)  # у объектов будет разная скорость
 
     def update(self):
         if self.rect.x < W:
@@ -52,19 +53,18 @@ class Car1(pygame.sprite.Sprite):
             self.kill()
 
 
-class UserCar(pygame.sprite.Sprite):
-    def __init__(self, x, surf):
+class UserSpaceship(pygame.sprite.Sprite):
+    def __init__(self, surf):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.rotate(surf, 0)
         self.image.set_colorkey((247, 247, 247))
         self.rect = self.image.get_rect(center=(300, 300))
 
 
-cars = pygame.sprite.Group()
+objects = pygame.sprite.Group()
 
-# добавляем первую машину, которая появляется сразу
-Car(randint(1, W), OBJ_SURF[randint(0, 2)], cars)
-user_car1 = UserCar(randint(1, W), pygame.image.load('./ufo.png'))
+SpaceObjectX(randint(1, W), OBJ_SURF[randint(0, 2)], objects)
+user_spaceship = UserSpaceship(pygame.image.load('./ufo.png'))
 
 while 1:
     for i in pygame.event.get():
@@ -73,9 +73,9 @@ while 1:
             break
         elif i.type == pygame.USEREVENT:
             if randint(0, 1) == 1:
-                Car(randint(1, W), OBJ_SURF[randint(0, 3)], cars)
+                SpaceObjectX(randint(1, W), OBJ_SURF[randint(0, 3)], objects)
             else:
-                Car1(randint(1, H), OBJ_SURF[randint(0, 3)], cars)
+                SpaceObjectY(randint(1, H), OBJ_SURF[randint(0, 3)], objects)
         if i.type == pygame.KEYDOWN:
             if i.key == pygame.K_LEFT:
                 motion = 'LEFT'
@@ -89,15 +89,15 @@ while 1:
                 motion = 'STOP'
 
     if motion == 'LEFT':
-        user_car1.rect[0] -= 2
+        user_spaceship.rect[0] -= 2
     elif motion == 'RIGHT':
-        user_car1.rect[0] += 2
+        user_spaceship.rect[0] += 2
     elif motion == 'UP':
-        user_car1.rect[1] -= 2
+        user_spaceship.rect[1] -= 2
     elif motion == 'DOWN':
-        user_car1.rect[1] += 2
+        user_spaceship.rect[1] += 2
 
-    if pygame.sprite.spritecollideany(user_car1, cars) is not None:
+    if pygame.sprite.spritecollideany(user_spaceship, objects) is not None:
         sc.fill(BLACK)
         sc.blit(font.render('Game is over :c', 1, (180, 0, 0)), (W // 3, H // 2))
         pygame.display.update()
@@ -105,9 +105,9 @@ while 1:
         break
     else:
         sc.fill((65, 74, 76))
-        sc.blit(user_car1.image, user_car1.rect)
-        cars.draw(sc)
+        sc.blit(user_spaceship.image, user_spaceship.rect)
+        objects.draw(sc)
         pygame.display.update()
-        cars.update()
+        objects.update()
 
-    pygame.time.delay(FPS)
+    clock.tick(FPS)
